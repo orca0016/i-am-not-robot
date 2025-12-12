b0a5572b6b3898a6454886ee50b4409f196e6631
Mohammad m <mashhadim901@gmail.com>
2025-12-12 14:26:23 +0330

    trrrr

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 echo "DEBUG ENV VARS:"
 echo "TELEGRAM_TOKEN is: $TELEGRAM_TOKEN"
-echo "OPENROUTER_KEY length: ${#OPENROUTER_KEY}"
+echo "OPENROUTER_KEY length:$OPENROUTER_KEY"
 
 echo "RUNNING BOT..."
 python main.py
